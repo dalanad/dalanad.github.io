f0a25a3e9b12d170cf0e59aa4927392a482cac19
Dalana Dharmathilake <dalana199981@gmail.com>
2023-12-22 11:20:08 +0530

    feat: initial version

---
+++ content/projects/AppleMusicEmbedPlayer.md
@@ -0,0 +1,16 @@
+---
+date: '2017-12-01'
+title: 'Placeholder Project'
+github: ''
+external: 'https://tools.applemusic.com/en-us'
+tech:
+  - MusicKit.js
+  - JS
+  - SCSS
+ios: 'https://apps.apple.com/us/app/apple-music/id1108187390'
+android: 'https://apps.apple.com/us/app/apple-music/id1108187390'
+company: 'Apple'
+showInProjects: false
+---
+
+Embeddable web player widget for Apple Music that lets users log in and listen to full song playback in the browser leveraging [MusicKit.js](https://developer.apple.com/documentation/musickitjs). Read more about this project on [9to5Mac](https://9to5mac.com/2018/06/03/apple-music-embeddable-web-player-listen-browser/).
---
+++ src/components/sections/hero.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { navDelay, loaderDelay } from '@utils';
+import { usePrefersReducedMotion } from '@hooks';
+import { email } from '@config';
+import Typewriter from 'typewriter-effect';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  height: 100vh;
+  padding: 0;
+
+  @media (max-height: 700px) and (min-width: 700px), (max-width: 360px) {
+    height: auto;
+    padding-top: var(--nav-height);
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  .big-heading div {
+    display: inline;
+  }
+
+  h3 {
+    margin-top: 5px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 600px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Hi, my name is</h1>;
+  const two = <h2 className="big-heading">Dalana Dhamrmathilake.</h2>;
+  const three = (
+    <h3 className="big-heading">
+      I build things{' '}
+      <Typewriter
+        style={{ display: 'inline' }}
+        onInit={typewriter => {
+          typewriter
+            .pasteString('')
+            .typeString('for people')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('on the web')
+            .pauseFor(4000)
+            .deleteAll()
+            .start();
+        }}
+        options={{ loop: true }}
+      />
+    </h3>
+  );
+  const four = (
+    <>
+      <p>
+        I'm a software engineer specializing in building web based solutions.
+        <br /> With a unique combination of technical skills and a passion for problem-solving, I'm
+        dedicated to taking on new challenges and continually expanding my expertise in software
+        development .
+      </p>
+    </>
+  );
+  const five = (
+    <a className="email-link" href={`mailto:${email}`}>
+      Say Hello
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      {prefersReducedMotion ? (
+        <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      )}
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
